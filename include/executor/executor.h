709088712b1cf7f067bc65f05328922cbb547794
Daigo Fujii <60760935+funera1@users.noreply.github.com>
2025-06-28 16:31:45 +0900

    Update include/executor/executor.h

--- include/executor/executor.h
+++ include/executor/executor.h
@@ -936,7 +936,6 @@ private:
   std::atomic_uint32_t StopToken = 0;
   /// Executor Host Function Handler
   HostFuncHandler HostFuncHelper = {};
-  // Migrator Migr;
   bool RestoreFlag = true;
 };
 
