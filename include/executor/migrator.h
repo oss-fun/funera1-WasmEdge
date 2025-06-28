ddd41076b84dccd02f648ba3c5f782189087727c
Daigo Fujii <60760935+funera1@users.noreply.github.com>
2025-06-28 16:31:28 +0900

    Update include/executor/migrator.h

--- include/executor/migrator.h
+++ include/executor/migrator.h
@@ -47,7 +47,6 @@ public:
   uint32_t getFuncIdx(const AST::InstrView::iterator PC);
   std::pair<uint32_t, uint32_t> getInstrAddrExpr(const Runtime::Instance::ModuleInstance *ModInst, AST::InstrView::iterator PC);
   std::vector<uint8_t> getTypeStack(uint32_t FuncIdx, uint32_t Offset, bool IsRetAddr);
-  std::vector<uint8_t> getTypeStack_v2(uint32_t FuncIdx, uint32_t Offset);
   bool isExistTypeStackTable();
 
   void debugFrame(uint32_t FrameIdx, uint32_t EnterFuncIdx, uint32_t Locals, uint32_t Arity, uint32_t VPos);
