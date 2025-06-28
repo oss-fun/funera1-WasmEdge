9dd9b4212290f82f2d8dc157a823aa9e91057867
funera1 <funeral7776@gmail.com>
2025-06-28 16:58:03 +0900

    Fix

--- lib/executor/engine/engine.cpp
+++ lib/executor/engine/engine.cpp
@@ -136,8 +136,6 @@ Executor::runFunction(Runtime::StackManager &StackMgr,
       Migr.dumpStack(StackMgr, StartIt);
       std::cerr << "Success dumpStack" << std::endl;
 
-      // NOTE: 復元したPCから1進めないと、次の命令から始まらないので+1
-      StartIt++;
       RestoreFlag = false;
     }
 
@@ -2254,15 +2252,6 @@ Expect<void> Executor::execute(Runtime::StackManager &StackMgr,
       }
     }
 
-    // OpCode Code = PC->getOpCode();
-    // std::cout << "[DEBUG]OpCode: 0x" << std::hex << (uint16_t)Code << std::dec << std::endl;
-    if (auto Res = Dispatch(); !Res) {
-      // SourceLoc PCSourceLoc = Migr.getSourceLoc(PC);
-      // std::cout << "[WASMEDGE ERROR] PC is " << PCSourceLoc.FuncIdx << " " << PCSourceLoc.Offset << std::endl;
-      // InteractiveMode(breakpoint, PCSourceLoc, StackMgr);
-      return Unexpect(Res);
-    }
-
     /* NOTE
         DumpFlag: checkpointシグナルを受け取ったときに1が代入される。受け取るまでは0が入る。
         isDumpMode: --no-checkpointオプションがない場合に1、ある場合に0が入る 
@@ -2304,6 +2293,16 @@ Expect<void> Executor::execute(Runtime::StackManager &StackMgr,
       exit(1);
       return {};
     }
+
+    // OpCode Code = PC->getOpCode();
+    // std::cout << "[DEBUG]OpCode: 0x" << std::hex << (uint16_t)Code << std::dec << std::endl;
+    if (auto Res = Dispatch(); !Res) {
+      // SourceLoc PCSourceLoc = Migr.getSourceLoc(PC);
+      // std::cout << "[WASMEDGE ERROR] PC is " << PCSourceLoc.FuncIdx << " " << PCSourceLoc.Offset << std::endl;
+      // InteractiveMode(breakpoint, PCSourceLoc, StackMgr);
+      return Unexpect(Res);
+    }
+
 
     PC++;
   }
